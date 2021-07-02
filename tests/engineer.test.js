de99c919299d14c86114d623252215b872c3610c
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-02 13:18:19 -0500

    debugging initial engineer test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,2 +1,15 @@
 // dependencies
 const Engineer = require('../lib/engineer');
+
+//initialize testing
+
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        //object should return an empty string
+        it("should initiate an object", () => {
+            const obj = new Engineer();
+            //We expect 'result' to equal object
+            expect(typeof(obj)).toEqual("object");
+        });
+    });
+});
