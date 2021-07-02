f09bb0c694a028356f479f7fc96d018420d1a53d
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-02 13:54:46 -0500

    create engineer test and passed all tests

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -12,4 +12,26 @@ describe("Engineer", () => {
             expect(typeof(obj)).toEqual("object");
         });
     });
+     //create test for github object
+    describe("Github", () => {
+        it("should create new github", () => {
+          const obj = new Engineer("Billy",12, "billy.email@email", "ChainRxn12");
+          expect(obj.github).toEqual("ChainRxn12");
+        });
+      });
+          //create test for getGithub method
+    describe("getGithub", () => {
+        it("can return gitHub username by getGithub method", () => {
+          const obj = new Engineer("Billy", 12, "billy.email@email", "ChainRxn12");
+          expect(obj.getGithub()).toEqual("ChainRxn12");
+        });
+      });
+      //create test for getRole method
+    describe("getRole", () => {
+        it("can return role by getRole method", () => {
+          const role = "Engineer";//must define role
+          const obj = new Engineer("Billy", 12, "billy.email@email", "ChainRxn12");
+          expect(obj.getRole()).toEqual(role);
+        });
+      });
 });
